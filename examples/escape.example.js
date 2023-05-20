fcd1fc65d921e04b0c1bae8e72250a3f2770919e
Sören Metje <soerenmetje@yahoo.de>
2023-05-20 17:37:55 +0200

    Add example

---
+++ examples/escape.example.js
@@ -0,0 +1,19 @@
+const escapeInput = require("../lib");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+
+async function main()  {
+    // [Receive request with userInput]
+
+    //const userInput = "124" // usual input
+    const userInput = "124' OR created>0 OR status:'active"  // injection
+
+    let subscriptions = await stripe.subscriptions.search({
+        query: `metadata['myField']: '${escapeInput(userInput)}'`
+    });
+    console.log(subscriptions) // all subscriptions ever due to injection
+
+    // [Serve subscriptions to user]
+}
+
+main()
