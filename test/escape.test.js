d9e97555748dfc64affa50e0c8454e0cd7bf60cd
Sören Metje <soerenmetje@yahoo.de>
2023-05-20 17:39:58 +0200

    Shorten function name in test

--- test/escape.test.js
+++ test/escape.test.js
@@ -1,12 +1,12 @@
-const escapeStripeSearchQueryInput = require('../lib');
+const escapeInput = require('../lib');
 
 test('valid input', () => {
-    expect(escapeStripeSearchQueryInput('foo')).toBe('foo');
+    expect(escapeInput('foo')).toBe('foo');
 });
 
 test('injection input using single quotes', () => {
-    expect(escapeStripeSearchQueryInput("124' OR created>0 OR status:'active")).toBe("124\\' OR created>0 OR status:\\'active");
+    expect(escapeInput("124' OR created>0 OR status:'active")).toBe("124\\' OR created>0 OR status:\\'active");
 });
 test('injection input using double quotes', () => {
-    expect(escapeStripeSearchQueryInput('124" OR created>0 OR status:"active')).toBe('124\\" OR created>0 OR status:\\"active');
+    expect(escapeInput('124" OR created>0 OR status:"active')).toBe('124\\" OR created>0 OR status:\\"active');
 });
