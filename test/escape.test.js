eab5b99506173f60fe6054e768aa0e39776b94b2
Sören Metje <soerenmetje@yahoo.de>
2023-05-23 03:17:39 +0200

    Fix missing escape of backslash (escape character)

--- test/escape.test.js
+++ test/escape.test.js
@@ -10,3 +10,17 @@ test('injection input using single quotes', () => {
 test('injection input using double quotes', () => {
     expect(escapeInput('124" OR created>0 OR status:"active')).toBe('124\\" OR created>0 OR status:\\"active');
 });
+
+test('injection input using single quotes with 1 backslash in front', () => {
+    expect(escapeInput("124\\' OR created>0 OR status:\\'active")).toBe("124\\\\\\' OR created>0 OR status:\\\\\\'active");
+});
+test('injection input using double quotes with 1 backslash in front', () => {
+    expect(escapeInput('124\\" OR created>0 OR status:\\"active')).toBe('124\\\\\\" OR created>0 OR status:\\\\\\"active');
+});
+
+test('injection input using single quotes with 2 backslashes in front', () => {
+    expect(escapeInput("124\\\\' OR created>0 OR status:\\\\'active")).toBe("124\\\\\\\\\\' OR created>0 OR status:\\\\\\\\\\'active");
+});
+test('injection input using double quotes with 2 backslashes in front', () => {
+    expect(escapeInput('124\\\\" OR created>0 OR status:\\\\"active')).toBe('124\\\\\\\\\\" OR created>0 OR status:\\\\\\\\\\"active');
+});
